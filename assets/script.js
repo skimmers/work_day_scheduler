d2f5e672f360fd9bdaeec9dabe5f001653136df2
Sharon Kim <skimilk19@gmail.com>
2021-01-20 20:28:43 -0500

    added hour class to html and added date/time

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    //creating the date, got code from moment.js
+    var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("currentDay").append(today);
+    console.log(today);
+
+    function colorChange() {
+        //creating hour
+        
+    }
+}
+
+)
